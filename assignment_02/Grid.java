aadc32792e35aa8f2f6c83b35a61a18dd75c3ff0
Stilianos Papaspiros <stelios.papaspiros@gmail.com>
2025-03-26 19:59:51 +0200

    winning condition check

--- assignment_02/Grid.java
+++ assignment_02/Grid.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 public class Grid {
     private char grid[][];
+    private int row;
+    private int col;
 
     public Grid(){
         grid = new char[4][3];
@@ -16,22 +18,127 @@ public class Grid {
 
     public void place(char c, int row, int col){
         grid[row][col] = c;
+
+        this.row = row;
+        this.col = col;
     }
 
-    public boolean isWin(){
+    public boolean wins(){
         String win = "";
 
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 3; j++){
-                if(grid[i][j] == ' '){
-                    continue;
-                }
-
-                win += grid[i][j];
-
-                if()
+        /**
+         * check right
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.col + i > 3 || grid[this.col + i][this.row] == ' '){
+                break;
             }
+            win += grid[this.col + i][this.row];
         }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check left
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.col - i < 0 || grid[this.col - i][this.row] == ' '){
+                break;
+            }
+            win += grid[this.col - i][this.row];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check down
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.row + i > 2 || grid[this.col][this.row + i] == ' '){
+                break;
+            }
+            win += grid[this.col][this.row + i];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check up
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.row - i < 0 || grid[this.col][this.row - i] == ' '){
+                break;
+            }
+            win += grid[this.col][this.row - i];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check right and up
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.col + i > 3 || this.row - i < 0 || grid[this.col + i][this.row - i] == ' '){
+                break;
+            }
+            win += grid[this.col + i][this.row - i];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check right and down
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.col + i > 3 || this.row + i > 2 || grid[this.col + i][this.row + i] == ' '){
+                break;
+            }
+            win += grid[this.col + i][this.row + i];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check left and up
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.col - i < 0 || this.row - i < 0 || grid[this.col - i][this.row - i] == ' '){
+                break;
+            }
+            win += grid[this.col - i][this.row - i];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        /**
+         * check left and down
+         */
+        win += grid[this.col][this.row];
+        for(int i = 1; i < 3; i++){
+            if(this.col - i < 0 || this.row + i > 2 || grid[this.col - i][this.row + i] == ' '){
+                break;
+            }
+            win += grid[this.col - i][this.row + i];
+        }
+        if(win.equals("XOX") || win.equals("OXO")){
+            return true;
+        }
+
+        return false;
     }
 
     public boolean isFull(){
--- assignment_02/xoxo.java
+++ assignment_02/xoxo.java
@@ -1,3 +1,11 @@
+package assignment_02;
+
 public class xoxo {
-    
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+        Player max = new Player('X');
+        Player min = new Player('O');
+
+                
+    }
 }
