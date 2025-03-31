5597b7d4214aa1b666d5aa8dccc603b93abdebc3
Stilianos Papaspiros <stelios.papaspiros@gmail.com>
2025-03-31 20:12:53 +0300

    MiniMax beta 1.0

--- assignment_02/GameLogic.java
+++ assignment_02/GameLogic.java
@@ -1,166 +1,83 @@
 package assignment_02;
 
-import java.util.Arrays;
+import java.util.Random;
 
 public class GameLogic {
     private char[][] grid;
     private int row;
     private int col;
+    private boolean maxFirst;
+    private boolean gameEnded;
 
     public GameLogic(){
-        grid = new char[4][3];
-        Arrays.fill(grid, ' ');
+        grid = new char[3][4];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                grid[i][j] = ' ';
+            }
+        }
+
+        Random rand = new Random(System.currentTimeMillis());
+
+        int rand_int1 = rand.nextInt(13);
+        int xrow = rand_int1 % 3;
+        int xcol = rand_int1 / 4;
+        int rand_int2 = rand.nextInt(13);
+        int orow = rand_int2 % 3;
+        int ocol = rand_int2 / 4;
+        while(orow == xrow && ocol == xcol || orow == xrow + 1 && ocol == xcol || orow == xrow - 1 && ocol == xcol || orow == xrow && ocol == xcol + 1 ||
+                orow == xrow && ocol == xcol - 1 || orow == xrow + 1 && ocol == xcol + 1 || orow == xrow - 1 && ocol == xcol + 1 ||
+                orow == xrow - 1 && ocol == xcol - 1 || orow == xrow + 1 && ocol == xcol - 1){
+            rand_int2 = rand.nextInt(13);
+            orow = rand_int2 % 3;
+            ocol = rand_int2 / 4;
+        }
+
+        this.grid[rand_int1 % 3][rand_int1 / 4] = 'X';
+        this.grid[rand_int2 % 3][rand_int2 / 4] = 'O';
+
+        int rand_int3 = rand.nextInt(1001);
+        if(rand_int3 > 500){
+            maxFirst = true;
+        }
+        else{
+            maxFirst = false;
+        }
+
+        gameEnded = false;
     }
 
     public char[][] getGrid(){
         return grid;
     }
 
-    public int evaluate(char[][] grid){
-        String win = "";
-
-        /**
-         * check right
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.col + i > 3 || grid[this.col + i][this.row] == ' '){
-                break;
-            }
-            win += grid[this.col + i][this.row];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check left
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.col - i < 0 || grid[this.col - i][this.row] == ' '){
-                break;
-            }
-            win += grid[this.col - i][this.row];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check down
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.row + i > 2 || grid[this.col][this.row + i] == ' '){
-                break;
-            }
-            win += grid[this.col][this.row + i];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check up
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.row - i < 0 || grid[this.col][this.row - i] == ' '){
-                break;
-            }
-            win += grid[this.col][this.row - i];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check right and up
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.col + i > 3 || this.row - i < 0 || grid[this.col + i][this.row - i] == ' '){
-                break;
-            }
-            win += grid[this.col + i][this.row - i];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check right and down
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.col + i > 3 || this.row + i > 2 || grid[this.col + i][this.row + i] == ' '){
-                break;
-            }
-            win += grid[this.col + i][this.row + i];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check left and up
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.col - i < 0 || this.row - i < 0 || grid[this.col - i][this.row - i] == ' '){
-                break;
-            }
-            win += grid[this.col - i][this.row - i];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        /**
-         * check left and down
-         */
-        win += grid[this.col][this.row];
-        for(int i = 1; i < 3; i++){
-            if(this.col - i < 0 || this.row + i > 2 || grid[this.col - i][this.row + i] == ' '){
-                break;
-            }
-            win += grid[this.col - i][this.row + i];
-        }
-        if(win.equals("XOX")){
-            return 2;
-        }
-        if(win.equals("OXO")){
-            return -2;
-        }
-
-        return 0;
+    public boolean isMaxFirst(){
+        return maxFirst;
     }
 
-    public boolean isFull(){
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 3; j++){
+    public boolean isGameEnded(){
+        if(this.isGridFull()){
+            gameEnded = true;
+        }
+
+        return gameEnded;
+    }
+
+    public boolean coordsValid(int row, int col){
+        if(this.grid[row][col] == ' '){
+            return true;
+        }
+
+        return false;
+    }
+
+    public void play(int row, int col){
+        this.grid[row][col] = 'O';
+    }
+
+    public boolean isGridFull(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
                 if(grid[i][j] == ' '){
                     return false;
                 }
@@ -170,15 +87,170 @@ public class GameLogic {
     }
 
     public void printGrid(){
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 3; j++){
-                System.out.print(grid[i][j] + " ");
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print("| " + grid[i][j] + " |");
             }
-            System.out.println();
+            System.out.println("\n--------------------");
         }
     }
 
-    public int minimax(char[][] grid, int depth, boolean isMax){
-        int score = evaluate(grid); 
+    public int evaluate(char[][] grid){
+        String win = "";
+
+        /*
+         * Check horizontally
+         */
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                if(grid[i][j] == ' '){
+                    win = "";
+                    continue;
+                }
+                win += grid[i][j];
+                if(win.equals("XOX")){
+                    return 2;
+                }
+                if(win.equals("OXO")){
+                    return -2;
+                }
+            }
+            win = "";
+        }
+        win = "";
+
+        /*
+         * Check vertically
+         */
+        for(int j = 0; j < 4; j++){
+            for(int i = 0; i < 3; i++){
+                if(grid[i][j] == ' '){
+                    win = "";
+                    continue;
+                }
+                win += grid[i][j];
+                if(win.equals("XOX")){
+                    return 2;
+                }
+                if(win.equals("OXO")){
+                    return -2;
+                }
+            }
+            win = "";
+        }
+        win = "";
+
+        /*
+         * Check diagonally (\)
+         */
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 3; j++){
+                if(grid[j][j+i] == ' '){
+                    win = "";
+                    continue;
+                }
+                win += grid[j][2-j+i];
+                if(win.equals("XOX")){
+                    return 2;
+                }
+                if(win.equals("OXO")){
+                    return -2;
+                }
+            }
+            win = "";
+        }
+        win = "";
+
+
+        /*
+         * Check diagonally (/)
+         */
+        for(int i = 0; i < 2; i++){
+            for(int j = 2; j >= 0; j--){
+                if(grid[j][2-j+i] == ' '){
+                    win = "";
+                    continue;
+                }
+                win += grid[j][2-j+i];
+                if(win.equals("XOX")){
+                    return 2;
+                }
+                if(win.equals("OXO")){
+                    return -2;
+                }
+            }
+            win = "";
+        }
+        win = "";
+
+        if(this.isGridFull()){
+            return 0;
+        }
+
+        return -1;
+    }
+    
+    public int minimax(char[][] grid, boolean isMax){
+        int score = evaluate(grid);
+
+        if(score != -1){
+            return score;
+        }
+        
+        if(isMax){
+            int bestScore = Integer.MIN_VALUE;
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 4; j++){
+                    if(grid[i][j] == ' '){
+                        grid[i][j] = 'X';
+                        bestScore = Math.max(bestScore, minimax(grid, !isMax));
+                        grid[i][j] = ' ';
+                    }
+                }
+            }
+            
+            return bestScore;
+        }
+        else{
+            int bestScore = Integer.MAX_VALUE;
+            for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 4; j++){
+                    if(grid[i][j] == ' '){
+                        grid[i][j] = 'O';
+                        bestScore = Math.min(bestScore, minimax(grid, !isMax));
+                        grid[i][j] = ' ';
+                    }
+                }
+            }
+        
+            return bestScore;
+        }
+    }
+
+    public void findBestMove(){
+        int bestValue = Integer.MIN_VALUE;
+        int bestRow = -1;
+        int bestCol = -1;
+        
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                if(grid[i][j] == ' '){
+                    this.row = i;
+                    this.col = j;
+                    grid[i][j] = 'X';
+                    int moveValue = minimax(grid, false);
+                    grid[i][j] = ' ';
+                    
+                    if(moveValue > bestValue){
+                        bestValue = moveValue;
+                        bestRow = i;
+                        bestCol = j;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Best move: (" + bestRow + ", " + bestCol + ")");
+        grid[bestRow][bestCol] = 'X';
     }
 }
--- assignment_02/xoxo.java
+++ assignment_02/xoxo.java
@@ -1,11 +1,69 @@
 package assignment_02;
 
+import java.util.Scanner;
+
 public class xoxo {
     public static void main(String[] args) {
-        Grid grid = new Grid();
-        Player max = new Player('X');
-        Player min = new Player('O');
+        Scanner sc = new Scanner(System.in);
+        GameLogic gl = new GameLogic();
 
-                
+        int row = -1;
+        int col = -1;
+
+        while(!gl.isGameEnded()){
+            gl.printGrid();
+            
+            System.out.println("Choose your cell (row -> enter -> column -> enter):\n");
+            row = sc.nextInt();
+            col = sc.nextInt();
+            
+            while(!gl.coordsValid(row, col)){
+                System.out.println("Choose an empty cell!\n");
+                gl.printGrid();
+                System.out.println("Choose your cell (row -> enter -> column -> enter):\n");
+                row = sc.nextInt();
+                col = sc.nextInt();
+            }
+            
+            gl.play(row, col);
+
+            if(gl.evaluate(gl.getGrid()) == -2){
+                System.out.println("User Wins!");
+                gl.printGrid();
+                return;
+            }
+            else if(gl.evaluate(gl.getGrid()) == 2){
+                System.out.println("Computer Wins!");
+                gl.printGrid();
+                return;
+            }
+            else if(gl.evaluate(gl.getGrid()) == 0){
+                System.out.println("Draw!");
+                gl.printGrid();
+                return;
+            }
+            
+            gl.printGrid();
+            
+            gl.findBestMove();
+            
+            if(gl.evaluate(gl.getGrid()) == -2){
+                System.out.println("User Wins!");
+                gl.printGrid();
+                return;
+            }
+            else if(gl.evaluate(gl.getGrid()) == 2){
+                System.out.println("Computer Wins!");
+                gl.printGrid();
+                return;
+            }
+            else if(gl.evaluate(gl.getGrid()) == 0){
+                System.out.println("Draw!");
+                gl.printGrid();
+                return;
+            }
+        }
+
+        gl.printGrid();
     }
 }
